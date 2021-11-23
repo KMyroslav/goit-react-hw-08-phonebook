1b4af53b0158ec6f2350493aa4b1565362db9f7c
KMyroslav <kukhtarukm@gmail.com>
2021-11-23 20:54:41 +0200

    Adds clear button

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -2,197 +2,196 @@ import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import selectors from "../redux/selectors";
+var orbs = [];
 
 export default function HomePage() {
   const isLoggedIn = useSelector(selectors.getIsLoggedIn);
   const userName = useSelector(selectors.getUserName);
   const navigate = useNavigate();
 
+  function clear() {
+    orbs = [];
+  }
+
   useEffect(() => {
     const bodyEl = document.body;
     bodyEl.style.backgroundColor = "#000";
     bodyEl.style.overflow = "hidden";
 
-    if (document.getElementById("c")) {
-      let intervalID,
-        timeoutID,
-        animationID,
-        loopAnimationId = null;
-      animationID = window.requestAnimFrame = (function () {
-        return (
-          window.requestAnimationFrame ||
-          window.webkitRequestAnimationFrame ||
-          window.mozRequestAnimationFrame ||
-          window.oRequestAnimationFrame ||
-          window.msRequestAnimationFrame ||
-          function (a) {
-            timeoutID = window.setTimeout(a, 1e3 / 60);
-          }
-        );
-      })();
-
-      document.onselectstart = function () {
-        return false;
-      };
-
-      var c = document.getElementById("c");
-      var ctx = c.getContext("2d");
-      var dpr = window.devicePixelRatio;
-      var cw = window.innerWidth;
-      var ch = window.innerHeight;
-      c.width = cw * dpr;
-      c.height = ch * dpr;
-      ctx.scale(dpr, dpr);
-      var rand = function (rMi, rMa) {
-        return ~~(Math.random() * (rMa - rMi + 1) + rMi);
-      };
-      ctx.lineCap = "round";
-      var orbs = [];
-
-      var trail = true;
-
-      function createOrb(mx, my) {
-        var dx = cw / 2 - mx;
-        var dy = ch / 2 - my;
-        var dist = Math.sqrt(dx * dx + dy * dy);
-        var angle = Math.atan2(dy, dx);
-        orbs.push({
-          x: mx,
-          y: my,
-          lastX: mx,
-          lastY: my,
-          hue: 0,
-          colorAngle: 0,
-          angle: angle + Math.PI / 2,
-          size: rand(1, 3) / 2,
-          centerX: cw / 2,
-          centerY: ch / 2,
-          radius: dist,
-          speed: (rand(5, 10) / 1000) * (dist / 750) + 0.015,
-          alpha: 1 - Math.abs(dist) / cw,
-          draw: function () {
-            ctx.strokeStyle = "hsla(" + this.colorAngle + ",100%,50%,1)";
-            ctx.lineWidth = this.size;
-            ctx.beginPath();
-            ctx.moveTo(this.lastX, this.lastY);
-            ctx.lineTo(this.x, this.y);
-            ctx.stroke();
-          },
-          update: function () {
-            var mx = this.x;
-            var my = this.y;
-            this.lastX = this.x;
-            this.lastY = this.y;
-            var x1 = cw / 2;
-            var y1 = ch / 2;
-            var x2 = mx;
-            var y2 = my;
-            var rise = y1 - y2;
-            var run = x1 - x2;
-            var slope = -(rise / run);
-            var radian = Math.atan(slope);
-            var angleH = Math.floor(radian * (180 / Math.PI));
-            if (x2 < x1 && y2 < y1) {
-              angleH += 180;
-            }
-            if (x2 < x1 && y2 > y1) {
-              angleH += 180;
-            }
-            if (x2 > x1 && y2 > y1) {
-              angleH += 360;
-            }
-            if (y2 < y1 && slope === "-Infinity") {
-              angleH = 90;
-            }
-            if (y2 > y1 && slope === "Infinity") {
-              angleH = 270;
-            }
-            if (x2 < x1 && slope === "0") {
-              angleH = 180;
-            }
-            if (isNaN(angleH)) {
-              angleH = 0;
-            }
-
-            this.colorAngle = angleH;
-            this.x = this.centerX + Math.sin(this.angle * -1) * this.radius;
-            this.y = this.centerY + Math.cos(this.angle * -1) * this.radius;
-            this.angle += this.speed;
-          },
-        });
-      }
-
-      function orbGo(e) {
-        var mx = e.pageX - c.offsetLeft;
-        var my = e.pageY - c.offsetTop;
-        createOrb(mx, my);
-      }
-
-      function turnOnMove() {
-        document.body.addEventListener("mousemove", orbGo, false);
-        document.body.addEventListener("touchmove", orbGo, false);
-      }
-
-      function turnOffMove() {
-        document.body.removeEventListener("mousemove", orbGo, false);
-        document.body.removeEventListener("touchmove", orbGo, false);
-      }
-
-      function clear() {
-        orbs = [];
-      }
-
-      document.body.addEventListener("mousedown", orbGo, false);
-      document.body.addEventListener("mousedown", turnOnMove, false);
-      document.body.addEventListener("mouseup", turnOffMove, false);
-      document.body.addEventListener("touchstart", orbGo, false);
-      document.body.addEventListener("touchstart", turnOnMove, false);
-      document.body.addEventListener("touchend", turnOffMove, false);
-      intervalID = setInterval(clear, 60000);
-
-      var count = 100;
-      while (count--) {
-        createOrb(cw / 2, ch / 2 + count * 6);
-      }
-
-      var loop = function () {
-        loopAnimationId = window.requestAnimFrame(loop);
-        if (trail) {
-          ctx.fillStyle = "rgba(0,0,0,.1)";
-          ctx.fillRect(0, 0, cw, ch);
-        } else {
-          ctx.clearRect(0, 0, cw, ch);
+    let intervalID,
+      timeoutID,
+      animationID,
+      loopAnimationId = null;
+    animationID = window.requestAnimFrame = (function () {
+      return (
+        window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        window.oRequestAnimationFrame ||
+        window.msRequestAnimationFrame ||
+        function (a) {
+          timeoutID = window.setTimeout(a, 1e3 / 60);
         }
-        var i = orbs.length;
-        while (i--) {
-          var orb = orbs[i];
-          var updateCount = 3;
-          while (updateCount--) {
-            orb.update();
-            orb.draw(ctx);
+      );
+    })();
+
+    document.onselectstart = function () {
+      return false;
+    };
+
+    var c = document.getElementById("c");
+    var ctx = c.getContext("2d");
+    var dpr = window.devicePixelRatio;
+    var cw = window.innerWidth;
+    var ch = window.innerHeight;
+    c.width = cw * dpr;
+    c.height = ch * dpr;
+    ctx.scale(dpr, dpr);
+    var rand = function (rMi, rMa) {
+      return ~~(Math.random() * (rMa - rMi + 1) + rMi);
+    };
+    ctx.lineCap = "round";
+
+    var trail = true;
+
+    function createOrb(mx, my) {
+      var dx = cw / 2 - mx;
+      var dy = ch / 2 - my;
+      var dist = Math.sqrt(dx * dx + dy * dy);
+      var angle = Math.atan2(dy, dx);
+      orbs.push({
+        x: mx,
+        y: my,
+        lastX: mx,
+        lastY: my,
+        hue: 0,
+        colorAngle: 0,
+        angle: angle + Math.PI / 2,
+        size: rand(1, 3) / 2,
+        centerX: cw / 2,
+        centerY: ch / 2,
+        radius: dist,
+        speed: (rand(5, 10) / 1000) * (dist / 750) + 0.015,
+        alpha: 1 - Math.abs(dist) / cw,
+        draw: function () {
+          ctx.strokeStyle = "hsla(" + this.colorAngle + ",100%,50%,1)";
+          ctx.lineWidth = this.size;
+          ctx.beginPath();
+          ctx.moveTo(this.lastX, this.lastY);
+          ctx.lineTo(this.x, this.y);
+          ctx.stroke();
+        },
+        update: function () {
+          var mx = this.x;
+          var my = this.y;
+          this.lastX = this.x;
+          this.lastY = this.y;
+          var x1 = cw / 2;
+          var y1 = ch / 2;
+          var x2 = mx;
+          var y2 = my;
+          var rise = y1 - y2;
+          var run = x1 - x2;
+          var slope = -(rise / run);
+          var radian = Math.atan(slope);
+          var angleH = Math.floor(radian * (180 / Math.PI));
+          if (x2 < x1 && y2 < y1) {
+            angleH += 180;
           }
-        }
-      };
+          if (x2 < x1 && y2 > y1) {
+            angleH += 180;
+          }
+          if (x2 > x1 && y2 > y1) {
+            angleH += 360;
+          }
+          if (y2 < y1 && slope === "-Infinity") {
+            angleH = 90;
+          }
+          if (y2 > y1 && slope === "Infinity") {
+            angleH = 270;
+          }
+          if (x2 < x1 && slope === "0") {
+            angleH = 180;
+          }
+          if (isNaN(angleH)) {
+            angleH = 0;
+          }
 
-      loop();
-
-      return () => {
-        bodyEl.style.backgroundColor = "#fff";
-        bodyEl.style.overflow = "visible";
-        window.clearInterval(intervalID);
-        window.clearTimeout(timeoutID);
-        window.cancelAnimationFrame(animationID);
-        window.cancelAnimationFrame(loopAnimationId);
-        document.body.removeEventListener("mousedown", orbGo, false);
-        document.body.removeEventListener("mousemove", orbGo, false);
-        document.body.removeEventListener("mousedown", turnOnMove, false);
-        document.body.removeEventListener("mouseup", turnOffMove, false);
-        document.body.removeEventListener("touchstart", orbGo, false);
-        document.body.removeEventListener("touchmove", orbGo, false);
-        document.body.removeEventListener("touchstart", turnOnMove, false);
-        document.body.removeEventListener("touchend", turnOffMove, false);
-      };
+          this.colorAngle = angleH;
+          this.x = this.centerX + Math.sin(this.angle * -1) * this.radius;
+          this.y = this.centerY + Math.cos(this.angle * -1) * this.radius;
+          this.angle += this.speed;
+        },
+      });
     }
+
+    function orbGo(e) {
+      var mx = e.pageX - c.offsetLeft;
+      var my = e.pageY - c.offsetTop;
+      createOrb(mx, my);
+    }
+
+    function turnOnMove() {
+      document.body.addEventListener("mousemove", orbGo, false);
+      document.body.addEventListener("touchmove", orbGo, false);
+    }
+
+    function turnOffMove() {
+      document.body.removeEventListener("mousemove", orbGo, false);
+      document.body.removeEventListener("touchmove", orbGo, false);
+    }
+
+    document.body.addEventListener("mousedown", orbGo, false);
+    document.body.addEventListener("mousedown", turnOnMove, false);
+    document.body.addEventListener("mouseup", turnOffMove, false);
+    document.body.addEventListener("touchstart", orbGo, false);
+    document.body.addEventListener("touchstart", turnOnMove, false);
+    document.body.addEventListener("touchend", turnOffMove, false);
+    intervalID = setInterval(clear, 60000);
+
+    var count = 100;
+    while (count--) {
+      createOrb(cw / 2, ch / 2 + count * 6);
+    }
+
+    var loop = function () {
+      loopAnimationId = window.requestAnimFrame(loop);
+      if (trail) {
+        ctx.fillStyle = "rgba(0,0,0,.1)";
+        ctx.fillRect(0, 0, cw, ch);
+      } else {
+        ctx.clearRect(0, 0, cw, ch);
+      }
+      var i = orbs.length;
+      while (i--) {
+        var orb = orbs[i];
+        var updateCount = 3;
+        while (updateCount--) {
+          orb.update();
+          orb.draw(ctx);
+        }
+      }
+    };
+
+    loop();
+
+    return () => {
+      bodyEl.style.backgroundColor = "#fff";
+      bodyEl.style.overflow = "visible";
+      window.clearInterval(intervalID);
+      window.clearTimeout(timeoutID);
+      window.cancelAnimationFrame(animationID);
+      window.cancelAnimationFrame(loopAnimationId);
+      document.body.removeEventListener("mousedown", orbGo, false);
+      document.body.removeEventListener("mousemove", orbGo, false);
+      document.body.removeEventListener("mousedown", turnOnMove, false);
+      document.body.removeEventListener("mouseup", turnOffMove, false);
+      document.body.removeEventListener("touchstart", orbGo, false);
+      document.body.removeEventListener("touchmove", orbGo, false);
+      document.body.removeEventListener("touchstart", turnOnMove, false);
+      document.body.removeEventListener("touchend", turnOffMove, false);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
@@ -245,7 +244,15 @@ export default function HomePage() {
         Or take some time to play with our magic background - tap anywhere on
         the screen
         <br />
-        (it reloads every minute)
+        (it will{" "}
+        <button
+          type="button"
+          className="badge badge-secondary"
+          onClick={() => clear()}
+        >
+          clear
+        </button>{" "}
+        every minute automaticaly)
       </p>
       <canvas id="c"></canvas>
     </div>
